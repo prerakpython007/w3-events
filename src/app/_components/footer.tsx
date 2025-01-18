f1677851e508701e5aac58ac11e46840328532b6
Prerak siddhpura <prerak.siddhpura@gmail.com>
2025-01-19 03:55:25 +0530

    .

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -9,16 +9,15 @@ const yatraOne = Yatra_One({
 
 const Footer = () => {
   return (
-    <footer className="w-full relative">
-      <div className="w-full">
+    <div className="">
         <Image 
           src="/div.png" 
           alt="" 
-          width={500} 
-          height={500}
-          className="w-full"
+          width={700} 
+          height={400}
+          className="w-[50%] m-auto"
         />
-      </div>
+      
       <div className=" pt-32 text-center">
 
         <h1 className={`${yatraOne.className} text-transparent  bg-clip-text bg-gradient-to-b from-[#F0F0F0] to-[#DA5B23] text-5xl z-20 `}>Be Among the First to <br /> Redefine Connection!</h1>
@@ -33,9 +32,49 @@ const Footer = () => {
         placeholder="soulspace@gmail.com"
         className="bg-[#0C0C0C] w-[50% ] h-10 rounded-lg border-2  border-[#4E2616] text-[#CAC9CE] pl-5"
         />
-        <button></button>
+        <div className="flex justify-center">
+        <button className="bg-[#DA5B23] p-2 rounded-xl mt-4" >Send Message</button>
+        </div>
+        
       </div>
-    </footer>
+      <hr className="w-[1300px] opacity-15 m-auto mt-36" />
+      <div className="grid grid-cols-2 w-[1300px]  pt-8">
+        <div className="flex space-x-8 pt-8">
+            <p>© 2024 Web3events</p>
+            <p>Privacy Policy</p>
+            <p>Terms of Use</p>
+        </div>
+        <div className="flex space-x-4 justify-end pt-8">
+            <Image
+            src={"/vector1.png"}
+            alt="img"
+            width={10}
+            height={100}
+            className="w-4 h-4"
+            />
+            <Image
+            src={"/vector2.png"}
+            alt="img"
+            width={100}
+            height={100}
+            className="w-4 h-4"
+            />
+            <Image
+            src={"/vector3.png"}
+            alt="img"
+            width={100}
+            height={100}
+            className="w-4 h-4"
+            />
+        </div>
+        <div className="pt-8 text-sm text-[#646464]">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        </div>
+
+
+      </div>
+    </div>
   );
 }
 
